2dbdf6446ffe33b4a50c79a8e268a1f8452a44a8
JYROH <ghgt1@naver.com>
2022-11-30 19:14:52 +0900

    refactor: Change Code logic
    ID를 모든 엘리먼트에부여해서 DOM으로 제어하는 코드를 간결하게
    renderTodo에서 모두할수있도록 변경함

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// API KEY 환경변수 설정
 require("dotenv").config();
 
 const API_URL = `https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos`;
@@ -15,29 +16,39 @@ const clearBtn = document.querySelector(".clear-btn");
 const alertStatus = document.querySelector(".status-alert");
 // 나중에 request하는 header와 body까지도 함수화하면 좋을듯.
 
+renderTodo();
+
+//alert 제어 함수
+function alertHandler(addClass, content) {
+  alertStatus.classList.add(addClass);
+  alertStatus.textContent = content;
+  setTimeout(() => {
+    alertStatus.classList.remove(addClass);
+    alertStatus.textContent = "";
+  }, 1000);
+}
+
+// todo추가
 inputForm.addEventListener("submit", (event) => {
   event.preventDefault();
   createTodo(inputData.value);
   inputData.value = null;
 });
 
+// todo전체삭제
 clearBtn.addEventListener("click", deleteTodoAll);
 
-renderTodoAll();
-
-// get List
-async function getTodoData() {
+// 전체 todo 받아오기
+async function getTodo() {
   const res = await fetch(API_URL, {
     method: "GET",
     headers: HEADERS,
   });
-  //   json은 응답데이터
   const json = await res.json();
-  console.log(json);
   return json;
 }
 
-// create List
+// todo 생성
 async function createTodo(content) {
   const res = await fetch(API_URL, {
     method: "POST",
@@ -46,123 +57,142 @@ async function createTodo(content) {
       title: content,
     }),
   });
-  //   json은 응답데이터
-  const json = await res.json();
-  console.log(json);
-  renderEachItem(json);
-
-  const editBtn = document.getElementById(`edit${json.id}`);
-  editBtn.addEventListener("click", editTodo);
-  const deleteBtn = document.getElementById(`delete${json.id}`);
-  deleteBtn.addEventListener("click", deleteEachTodo);
-
-  alertStatus.classList.add("alert-add");
-  alertStatus.textContent = "TODO 추가 완료";
-  setTimeout(() => {
-    alertStatus.classList.remove("alert-add");
-    alertStatus.textContent = "";
-  }, 1000);
+  alertHandler("alert-add", "TODO 추가 완료");
+  // 여기서 이벤트생성이 아닌 render all 어차피 서버에 정보는 있다
+  renderTodo();
 }
 
-// render item
-function renderEachItem(json) {
-  //inner html로 작성
+// todo 하나 렌더링 과정
+function renderEachItem(todo) {
   const todoSection = document.createElement("div");
-  todoSection.id = json.id;
   todoSection.classList.add("todos");
-  todoSection.innerHTML = /* html */ `
-<p class="title">${json.title}</p>
-	<div class="btn-container">
-		<button type="button" class="edit-btn" id=edit${json.id}>
-			<i class="fa-solid fa-pen-to-square"></i>
-		</button>
-		<button type="button" class="delete-btn" id=delete${json.id}>
-		<i class="fa-solid fa-trash"></i>
-		</button>
-	</div>
-`;
+
+  // left Container생성
+  const leftContainer = document.createElement("div");
+  leftContainer.classList.add("left-container");
+  const checkEl = document.createElement("input");
+  checkEl.type = "checkbox";
+  checkEl.checked = todo.done;
+  checkEl.id = todo.id;
+  // checkEl 이벤트리스너 생성
+  checkEl.addEventListener("click", () => {
+    editCheckbox(todo.id, todo.title, todo.done);
+  });
+  const titleEl = document.createElement("p");
+  titleEl.classList.add("title");
+  titleEl.textContent = todo.title;
+  if (todo.done) titleEl.style.textDecoration = "line-through";
+  leftContainer.append(checkEl, titleEl);
+
+  // btn Container 생성
+  const btnSection = document.createElement("div");
+  btnSection.classList.add("btn-container");
+  const timeEl = document.createElement("p");
+  timeEl.classList.add("title");
+  timeEl.textContent = `Last Updated: ${new Date(
+    todo.updatedAt
+  ).toLocaleString()}`;
+  const editBtn = document.createElement("button");
+  editBtn.type = "button";
+  editBtn.classList.add("edit-btn");
+  const editIcon = document.createElement("i");
+  editIcon.classList.add("fa-solid", "fa-pen-to-square");
+  editBtn.append(editIcon);
+  // editBtn 이벤트리스너 생성
+  editBtn.addEventListener("click", async () => {
+    await editContent(todo.id, todo.title, todo.done);
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.type = "button";
+  deleteBtn.classList.add("delete-btn");
+  const deleteIcon = document.createElement("i");
+  deleteIcon.classList.add("fa-solid", "fa-trash");
+  deleteBtn.append(deleteIcon);
+  //이벤트리스너 즉시실행문제 해결
+  deleteBtn.addEventListener("click", async () => {
+    await deleteEachTodo(todo.id);
+    alertHandler("alert-clear", "TODO 삭제 완료");
+    renderTodo();
+  });
+  btnSection.append(timeEl, editBtn, deleteBtn);
+  todoSection.append(leftContainer, btnSection);
   todoLists.append(todoSection);
 }
 
-// render List
-async function renderTodoAll() {
+// todo 전체 렌더링
+async function renderTodo() {
+  let json = await getTodo();
+  // json = json.reverse();
   todoLists.innerHTML = "";
-  let json = await getTodoData();
-  for (let todo of json) {
+  json.forEach((todo) => {
     renderEachItem(todo);
-  }
-  return json;
+  });
 }
 
-// 수정
-// 수정은 내용뿐만이아닌, 완료여부도 필요함
-async function editTodo() {
+//todo check 변경
+//찍긋기도 구현.
+//이건 전체 rendering안하고 방법은... dom으로 다루기?
+async function editCheckbox(todoId, todoTitle, todoDone) {
+  const checkEl = document.getElementById(todoId);
+  const checked = checkEl.checked;
+  const content = checkEl.nextElementSibling;
+  // 이게 다시 렌더링될때는 기억못하고 사라짐
+  if (checked) content.style.textDecoration = "line-through";
+  else content.style.textDecoration = "none";
+  const res = await fetch(`${API_URL}/${todoId}`, {
+    method: "PUT",
+    headers: HEADERS,
+    body: JSON.stringify({
+      title: todoTitle,
+      done: checked,
+    }),
+  });
+}
+
+//todo 내용 변경
+async function editContent(todoId, todoTitle, todoDone) {
   submitBtn.textContent = "Edit";
   inputData.value = null;
-  const parentItem = document.getElementById(this.id.slice(4, this.id.length));
+  inputData.placeholder = "변경할 내용을 입력해주세요";
+
   const editFunction = async (event) => {
     event.preventDefault();
-    const res = await fetch(`${API_URL}/${parentItem.id}`, {
+    const res = await fetch(`${API_URL}/${todoId}`, {
       method: "PUT",
       headers: HEADERS,
       body: JSON.stringify({
         title: inputData.value,
-        done: false,
+        done: todoDone,
       }),
     });
-    parentItem.childNodes[1].textContent = inputData.value;
     submitBtn.textContent = "Submit";
     inputData.value = null;
-    // eventlistner삭제
     submitBtn.removeEventListener("click", editFunction);
-    alertStatus.classList.add("alert-edit");
-    alertStatus.textContent = "TODO 수정 완료";
-    setTimeout(() => {
-      alertStatus.classList.remove("alert-edit");
-      alertStatus.textContent = "";
-    }, 1000);
+    alertHandler("alert-edit", "TODO 수정 완료");
+    renderTodo();
   };
+
   submitBtn.addEventListener("click", editFunction);
 }
 
-// 하나씩 삭제
-async function deleteEachTodo(todoId = "", all = true) {
-  // all은 전체삭제인지 판단하는것
-  let parentItem = "";
-  if (all) {
-    parentItem = document.getElementById(this.id.slice(6, this.id.length));
-    todoId = parentItem.id;
-    parentItem.remove();
-  }
+// todo 개별 삭제
+async function deleteEachTodo(todoId = "") {
   const res = await fetch(`${API_URL}/${todoId}`, {
     method: "DELETE",
     headers: HEADERS,
   });
-  if (all) {
-    alertStatus.classList.add("alert-clear");
-    alertStatus.textContent = "TODO 삭제 완료";
-    setTimeout(() => {
-      alertStatus.classList.remove("alert-clear");
-      alertStatus.textContent = "";
-    }, 1000);
-  }
 }
 
-// 전체 삭제
+// todo 전체 삭제
 async function deleteTodoAll() {
-  let json = await getTodoData();
+  let json = await getTodo();
   let promises = [];
-  for (let todo of json) {
-    promises.push(deleteEachTodo(todo.id, false));
-  }
+  // 좀더연구
+  json.forEach((todo) => {
+    promises.push(deleteEachTodo(todo.id));
+  });
   await Promise.all(promises);
-  // promise.all을 해봤지만 시간은 여전히 오래걸림....
-  // 꼼수로 html을 먼저 지우던가 해야하나?
-  alertStatus.classList.add("alert-clear");
-  alertStatus.textContent = "전체 삭제 완료";
-  setTimeout(() => {
-    alertStatus.classList.remove("alert-clear");
-    alertStatus.textContent = "";
-  }, 1000);
-  renderTodoAll();
+  alertHandler("alert-clear", "전체 삭제 완료");
+  renderTodo();
 }
